dfea64488264ac1085b85cb48a9ecd554a451c38
jmuteru <jeffmuteru@gmail.com>
2025-10-08 20:25:20 +0300

    Update:API docs

--- src/server.ts
+++ src/server.ts
@@ -117,7 +117,7 @@ mongoose
     process.exit(1);
   });
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5500;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}`);
